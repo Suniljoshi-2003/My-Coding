932b1587f4e8c946762911cf1e1ed8804175b3ab
Sunil <Suniljoshi6360@gmail.com>
2026-09-13 16:07:25 +0530

    Added comment

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,66 @@
 print("Hello World")
 print("How are you")
+"""
+1. Git start karo
+git init
+
+👉 Isse My Coding folder Git repository ban jaata hai.
+
+2. Git username set karo
+git config --global user.name "..."
+
+3. GitHub email set karo
+git config --global user.email "YOUR_GITHUB_EMAIL"
+
+👉 Yahan apna GitHub account wala email lagana.
+
+4. Files Git mein add karo
+git add .
+
+👉 . ka matlab current folder ki saari files.
+5. First commit
+git commit -m "First Commit"
+
+👉 Git tumhare current code ka ek version/save point bana leta hai.
+
+6. Main branch banao
+git branch -M main
+
+7. GitHub repository connect karo
+git remote add origin YOUR_GITHUB_REPOSITORY_URL
+
+Example:
+
+git remote add origin https://github.com/A/ab.git
+
+👉 Yahan apni actual GitHub repository URL lagani hai.
+
+8. GitHub par code bhejo 🚀
+git push -u origin main
+
+Iske baad tumhari files GitHub repository mein aa jayengi. ✅
+
+🔄 Aage se daily kaam
+
+Ab git init dobara nahi karna.
+
+Jab bhi VS Code mein code change karo:
+
+git add .
+git commit -m "Updated code"
+git push
+Yaad rakhne ka simple formula:
+
+Change code → Save → Add → Commit → Push
+
+VS Code
+   ↓
+git add .
+   ↓
+git commit
+   ↓
+git push
+   ↓
+GitHub
+
+"""
